5adebaef6a0421f74d65463234be3ef8fdce242e
Nicolas Leão <nicolasnleao@gmail.com>
2024-06-25 14:41:20 -0300

    use qdrant server to enable concurrent db access

--- chat.py
+++ chat.py
@@ -10,14 +10,14 @@ config = {}
 with open('config.json') as f:
     config = json.load(f)
 
-# Create Qdrant client and store
-client = qdrant_client.QdrantClient(path="./qdrant_data")
-vector_store = QdrantVectorStore(client=client, collection_name="polyrag_documents")
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
 # Initialize Ollama and ServiceContext
 Settings.llm = Ollama(model=config["llm"])
 Settings.embed_model = HuggingFaceEmbedding(model_name=config["embedding_model"])
+
+# Create Qdrant client and store
+client = qdrant_client.QdrantClient(url="http://localhost:6333")
+vector_store = QdrantVectorStore(client=client, collection_name="polyrag_documents")
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
 # Create VectorStoreIndex and query engine
 index = VectorStoreIndex.from_vector_store(
@@ -25,14 +25,13 @@ index = VectorStoreIndex.from_vector_store(
 )
 query_engine = index.as_query_engine()
 
-
 print('PolyRAG started, ask any questions about your documents or type \'bye\' to leave')
 # chat loop to perform a query and print the response
 running = True
 while running:
-    query = input('$ polyrag -> ')
+    query = input('$ you -> ')
     if query == 'quit' or query == 'exit' or query == 'bye' or query == 'close':
         running = False
     else:
         response = query_engine.query(query)
-        print(response)
+        print('$ polyrag -> ', response)
--- indexer.py
+++ indexer.py
@@ -4,30 +4,38 @@ import qdrant_client
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.extractors import TitleExtractor
+from llama_index.core.ingestion import IngestionPipeline
 import json
 
 config = {}
 with open('config.json') as f:
     config = json.load(f)
 
-# Load markdown and pdf documents
-reader = SimpleDirectoryReader(input_dir="./data", required_exts=[".pdf", ".md"])
-documents = reader.load_data()
-
-# Create Qdrant client and store
-client = qdrant_client.QdrantClient(path="./qdrant_data")
-vector_store = QdrantVectorStore(client=client, collection_name="polyrag_documents")
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
 # Initialize Ollama and ServiceContext
 Settings.llm = Ollama(model=config["llm"])
 Settings.embed_model = HuggingFaceEmbedding(model_name=config["embedding_model"])
 
-# Create VectorStoreIndex and query engine
-index = VectorStoreIndex.from_documents(
-    documents,
-    storage_context=storage_context,
+# Create Qdrant client and store
+client = qdrant_client.QdrantClient(url="http://localhost:6333")
+vector_store = QdrantVectorStore(client=client, collection_name="polyrag_documents")
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+# create the pipeline with transformations
+pipeline = IngestionPipeline(
+    transformations=[
+        SentenceSplitter(chunk_size=1024, chunk_overlap=64),
+        TitleExtractor(),
+        HuggingFaceEmbedding(model_name=config["embedding_model"]),
+    ],
+    vector_store=vector_store
 )
-query_engine = index.as_query_engine()
+
+# Load markdown and pdf documents
+reader = SimpleDirectoryReader(input_dir="./data", required_exts=[".pdf", ".md"])
+documents = reader.load_data()
+
+pipeline.run(documents=documents)
 
 print("Document indexing completed.")
